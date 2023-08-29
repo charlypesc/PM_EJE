c626b884623291d49ddb3e77a45b761588d06af3
charlypesc <cparedes.escobar@gmail.com>
2023-08-29 17:10:20 -0400

    :sparkles: se crean 2 pantallas y navegabilidad

--- app/src/main/java/com/example/ejercicio/Pantalla1Activity2.kt
+++ app/src/main/java/com/example/ejercicio/Pantalla1Activity2.kt
@@ -4,45 +4,42 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ejercicio.ui.theme.EjercicioTheme
 
-class MainActivity : ComponentActivity() {
+class Pantalla1Activity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-         PantallaInicio()
+            Pantalla1()
         }
     }
 }
-@Preview
+
+
+@Preview(showBackground = true)
 @Composable
-fun PantallaInicio() {
+fun Pantalla1() {
     val contexto = LocalContext.current
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ){
-            Button(onClick = {
-            val intento = Intent(contexto, Pantalla1Activity2::class.java)
-            contexto.startActivity(intento)
-        }){
-            Text("Pantalla 1")
-        }
-        Button(onClick = {
-
-        }) {
-            Text("Pantalla 2")
-        }
-    }
+        modifier = Modifier.fillMaxSize().background(Color.Cyan)
+    ){}
+       Button(onClick = {
+            val intento = Intent(contexto, MainActivity::class.java)
+            contexto.startActivity(intento);
+       }){
+            Text(text = "Volver")
+       }
 }
+
